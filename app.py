26e00cdf2a5c89ac90d0fdcbac82524a1b59140b
BearCut <161555447+BearCut@users.noreply.github.com>
2024-10-21 18:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ connection = sql.connect(
     host="127.0.0.1",
     port = 3306,
     user="root",
-    password="ItsNotRoot",
+    password="--passwordHere--",
     database="irs",
     autocommit="True"
 )
